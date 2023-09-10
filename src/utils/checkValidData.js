6465b12e05af1e0312929ceaa82e2a9584382218
BSB9186598 <bhaskarbhandari372@gmail.com>
2023-09-10 16:51:22 +0530

    changes

--- src/utils/checkValidData.js
+++ src/utils/checkValidData.js
@@ -1,7 +1,7 @@
 export const checkValidData = (email, password, name) => {
     const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
-    const isNameValid = /^[A-Za-z][A-Za-z0-9_]{7,29}$/.test(name)
+    const isNameValid = /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/.test(name)
 
 
     if(!isEmailValid) return "Email is not valid"
