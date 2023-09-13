b4525157f59a6ae7c5d6902b46b6f08855dad92a
BSB9186598 <bhaskarbhandari372@gmail.com>
2023-09-13 14:38:02 +0530

    bvaha

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCq_fzPlxM9pte6o2GSGIfuhk_rM5LH8Mg",
-  authDomain: "netflix-clone-15518.firebaseapp.com",
-  projectId: "netflix-clone-15518",
-  storageBucket: "netflix-clone-15518.appspot.com",
-  messagingSenderId: "157524520247",
-  appId: "1:157524520247:web:64c7220caa8ad937dcbeb2",
-  measurementId: "G-0MVP3BM9BL"
+  apiKey: "AIzaSyC3PXvRsu9gTC8syyRxSfae4u_SRDe5DXg",
+  authDomain: "netflix-gpt-ea06e.firebaseapp.com",
+  projectId: "netflix-gpt-ea06e",
+  storageBucket: "netflix-gpt-ea06e.appspot.com",
+  messagingSenderId: "336734302944",
+  appId: "1:336734302944:web:00b7146bf7c0fc84655685",
+  measurementId: "G-B6B7FJHZYH"
 };
 
 // Initialize Firebase
